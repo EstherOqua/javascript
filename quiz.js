fe2b13b6c513f246029e53be0cb13955b2df8f40
EstherOqua <66262441+EstherOqua@users.noreply.github.com>
2020-09-19 20:05:01 +0100

    CSS and Styling

--- quiz.js
+++ quiz.js
@@ -1,37 +1,7 @@
+// Variables
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
-
-function buildQuiz(){
-    const output = [];
-    const answers;
-
-    myQuestions.forEach(var i=0; i<questions.length; i++){
-        answers = [];
-        for(letter in questions[i].answers){
-            answers.push(
-                '<label>'
-                   <input type="radio" name="question${questionNumber}" value="${letter}">
-                    ${letter} :
-                    ${currentQuestion.answer[letter]}
-                '</label>'  
-            );
-        }
-
-        output.push(
-            <div class="answers">${answers.join('')}</div>'
-
-        );
-    }
-}
-function showResults(questions, quizContainer){}
-
-    // show the questions
-buildQuiz();
-
-    // when user clicks submit, show results
-submitButton.addEventListener('click', showResults);
-
 const myQuestions = [
     {
         question: 'What beauty spot was Dominic Cummings driving to test his eyesight after being infected with coronavirus?',
@@ -45,11 +15,11 @@ const myQuestions = [
     {
         question: "What is the name of the Netflix documentary about Joe Exotic?",
         answers: {
-            a:"Lion King",
-            b:"Tiger King",
-            c:"Jungle King",
+            a: "Lion King",
+            b: "Tiger King",
+            c: "Jungle King",
         },
-        correctAnswer:"b"
+        correctAnswer: "b"
     },
     {
         question: "On which social media platform did Prince Harry and Meghan Markle announce they were stepping down as senior royals?",
@@ -58,13 +28,13 @@ const myQuestions = [
             b: "Instagram",
             c: "Facebook",
         },
-        correctAnswer:"b"
+        correctAnswer: "b"
     },
     {
         question: "What colour is the circle on the London Underground logo?",
         answers: {
-            a:"Blue",
-            b:"White",
+            a: "Blue",
+            b: "White",
             c: "Red",
         },
         correctAnswer: "c"
@@ -78,4 +48,122 @@ const myQuestions = [
         },
         correctAnswer: "a"
     },
-]
+];
+function buildQuiz() {
+    // variable to store the HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+            // variable to store the list of possible answers
+            const answers = [];
+
+            // and for each available answer...
+            for (letter in currentQuestion.answers) {
+
+                // ...add an HTML radio button
+                answers.push(
+                    `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+                );
+            }
+            // add this question and its answers to the output
+            output.push(
+                `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+            );
+        }
+    );
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+}
+
+function showResults() {
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        // if answer is correct
+        if (userAnswer === currentQuestion.correctAnswer) {
+            // add to the number of correct answers
+            numCorrect++;
+
+            // color the answers green
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else {
+            // color the answers red
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if (currentSlide === 0) {
+        previousButton.style.display = 'none';
+    }
+    else {
+        previousButton.style.display = 'inline-block';
+    }
+    if (currentSlide === slides.length - 1) {
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+    }
+    else {
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+    }
+}
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+}
+
+
+// Kick things off
+buildQuiz();
+
+// Pagination
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+// Show the first slide
+showSlide(currentSlide);
+
+// Event listeners
+submitButton.addEventListener('click', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
