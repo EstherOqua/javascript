39b3006471073a1da577f845ed239c046a64d778
EstherOqua <66262441+EstherOqua@users.noreply.github.com>
2020-11-10 16:18:47 +0000

    Functions and Parameters

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+// used in index.html
+
+function sayHey() {
+    console.log("Hey" );
+}
+
+function conversation (person, job) {
+    return "Hey " + person + "!" + " Are you an " + job + "?";
+}
+
+
